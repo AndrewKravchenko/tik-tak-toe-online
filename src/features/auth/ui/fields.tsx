963322608250fc62476326ddb8336e56e416577a
Andrey <andreikrauchanka1994@gmail.com>
2025-04-04 19:51:36 +0300

    feat: sign-in validation

--- src/features/auth/ui/fields.tsx
+++ src/features/auth/ui/fields.tsx
@@ -2,7 +2,16 @@ import { Input } from '@/shared/ui/input'
 import { Label } from '@/shared/ui/label'
 import React, { useId } from 'react'
 
-export function AuthFields() {
+export function AuthFields({
+  errors,
+  formData,
+}: {
+  formData?: FormData
+  errors?: {
+    login?: string
+    password?: string
+  }
+}) {
   const loginId = useId()
   const passwordId = useId()
 
@@ -10,11 +19,28 @@ export function AuthFields() {
     <>
       <div className="space-y-2">
         <Label htmlFor={loginId}>Login</Label>
-        <Input id={loginId} type="login" name="login" placeholder="Enter your login" required />
+        <Input
+          id={loginId}
+          type="login"
+          name="login"
+          placeholder="Enter your login"
+          required
+          defaultValue={formData?.get('login')?.toString()}
+        />
+        {errors?.login && <div>{errors.login}</div>}
       </div>
+
       <div className="space-y-2">
         <Label htmlFor={passwordId}>Password</Label>
-        <Input id={passwordId} type="password" name="password" placeholder="Enter your password" required />
+        <Input
+          id={passwordId}
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+          defaultValue={formData?.get('password')?.toString()}
+        />
+        {errors?.password && <div>{errors.password}</div>}
       </div>
     </>
   )
